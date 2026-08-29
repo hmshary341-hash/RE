adef132a8bdaf7e20bfefed3ea4c3a159ff576ef
hmshary341-hash <hmshary341@gmail.com>
2026-08-29 18:42:40 +0300

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
+
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.app_commands.command(name="ping", description="فحص سرعة استجابة البوت")
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"Pong! 🏓 البوت شغال بنجاح.")
+
+async def setup(bot):
+    await bot.add_cog(General(bot))
 
